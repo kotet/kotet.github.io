3068976da6570ad0e55fb5dd2d313254979a7aa0
Kotet <kotet0418@gmail.com>
2019-05-04 20:38:42 +0900

    Fix loading bug

--- main.js
+++ main.js
@@ -55,8 +55,8 @@ window.addEventListener("load", function () {
     ctx = canvas.getContext("2d");
     ctx.globalCompositeOperation = "destination-atop";
     brush = new Image();
-    brush.src = BRUSHPATH + "?" + new Date().getTime();
     brush.onload = (() => { setTimeout(draw, 100); setInterval(resize, 1000); })();
+    brush.src = BRUSHPATH + "?" + new Date().getTime();
 });
 
 function bezier(a, b, c, d, t) {
