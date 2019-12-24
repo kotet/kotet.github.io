dff0679eba6f476efeb8bfb61c86d87fbdf37dd8
Kotet <kotet0418@gmail.com>
2019-12-24 17:09:42 +0900

    Simple

--- main.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("load", function () {
-    console.log("hi");
-    let me = document.getElementById("kotet");
-    let back = document.getElementById("body");
-    let h = Math.floor(Math.random() * 360);
-    me.style.backgroundColor = "hsl(" + h + ",70%,70%)";
-});
